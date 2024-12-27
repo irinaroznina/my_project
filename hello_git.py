b57908d96b851789dfe09da67c7dba205fed83bc
Irina Roznina <irinaroznina7@gmail.com>
2024-12-27 12:30:01 +0500

    Изменение файла hello_git.py (add while)

--- hello_git.py
+++ hello_git.py
@@ -1,2 +1,13 @@
-print('hello git')
-print("Indexing -i is impotant")
+import b
+print('Hello, Git')
+print('Indexing - it is important')
+
+ind = 0
+sum = 0
+
+while ind <= 9:
+        sum += ind
+        print(sum)
+        ind += 1
+
+print(b.any_func(2, sum))
